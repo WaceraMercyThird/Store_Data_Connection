258fff1bba6c23e01f51622a2984bb4f4417d5fc
Mercy Wacera <waceraaamercy@gmail.com>
2023-02-13 11:28:55 +0300

    Did Migration

---
+++ Base/Base/data/Models/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Base.data.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+public class ApplicationDBContext : DbContext
+{
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
+}
+
